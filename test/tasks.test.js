daad5e0d6169500e2380e1188cf6c736e6319bbf
Eva <106834827+BigPurpleVan@users.noreply.github.com>
2025-10-15 15:12:28 +0200

    test: createTask function testing

--- test/tasks.test.js
+++ test/tasks.test.js
@@ -1,9 +1,29 @@
-const { getTasks, reset } = require('../lib/tasks');
+const { getTasks, reset, createTask } = require('../lib/tasks');
 
 beforeEach(() => {
   reset();
 });
 
 test('initial task list is empty', () => {
+  expect(getTasks()).toEqual([]);
+});
+
+test('createTask adds a new task', () => {
+  createTask('Test task');
+  expect(getTasks()).toEqual([{ id: 1, name: 'Test task', done: false }]);
+});
+
+test('createTask increments task IDs', () => {
+  createTask('First task');
+  createTask('Second task');
+  expect(getTasks()).toEqual([
+    { id: 1, name: 'First task', done: false },
+    { id: 2, name: 'Second task', done: false }
+  ]);
+});
+
+test('reset clears the task list', () => {
+  createTask('Task to be cleared');
+  reset();
   expect(getTasks()).toEqual([]);
 });
